3ad9c4695b3be6cffb623db639ab5bcb6f39dd13
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-12-09 23:11:44 -0500

    Update urls.py

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,1 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'blogs'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('new/', views.new_post, name='new_post'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+]
+
+
 
